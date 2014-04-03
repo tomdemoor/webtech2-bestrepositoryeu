9aeb55be5879f98ee78623c40405151aa751e302
tommooredotbe <mooretom@outlook.com>
2014-04-03 02:08:34 +0200

    added commentary in script.js in what i need to do next. First try to understand the logic, then build it.

--- labo6_TomDeMoor/public/javascripts/script.js
+++ labo6_TomDeMoor/public/javascripts/script.js
@@ -1,30 +1,23 @@
-//msgid maken buiten doc.ready zodat deze voro elke gebruiker hetzelfde zijn
-//var postID = 0;
-
 $(document).ready(function(){
+  var postID = 0;
+  var postIdCollection = [];
 
-	//client aanmaken
-	//zoals op http://faye.jcoglan.com/node/clients.html
+	//client aanmaken zoals op http://faye.jcoglan.com/node/clients.html
 	var client = new Faye.Client('http://localhost:3000/faye/',{
 				timeout: 20
 	});
 
-	//subscribe, vraag printen
-	//http://faye.jcoglan.com/browser/subscribing.html
+	//subscribe en vraag printen http://faye.jcoglan.com/browser/subscribing.html
 	var postSubscribtion = client.subscribe('/post', function(message) {
-  		// handle messages
-      //voeg ID aan messages toe om ze uniek te maken
-      //id='p" + postID + "'
-  		$(".bulletin").append("<p class='newspost'><span class='user'>" + message.user + "</span><span class='posted'> posted:</span></br>" + message.post + "</br><div class='vote'>vote</div></p>")
-	});
-
-  var voteSubscribtion = client.subscribe('/vote', function(btn) {
-      //apply effect on parent
-      $(btn.vote).css("color","red");
+  		//handle messages en voeg ID aan messages toe om ze uniek te maken
+  		var newsPost = $(".bulletin").append("<p class='newspost' id='p" + postID + "'><span class='user'>" + message.user + "</span><span class='posted'> posted:</span></br>" + message.post + "</br><div class='vote' id='v" + postID + "'>vote</div></p>")
+	    postID++;
+      //id's in array zodat we ze later kunnen gebruiken bij andere gebruikers
+      postIdCollection.push("p" + postID);
   });
 
   	//onclick subscribe / publish
-  $('#send').on('click', function(){
+    $('#send').on('click', function(){
   			//haal values op
   			var userVal = $('#user').val();
   			var postVal = $('#question').val();
@@ -41,21 +34,27 @@ $(document).ready(function(){
     		$(".error").hide();
     		$('#question').val("");
 			  var postPublication = client.publish('/post', {post : postVal, user : userVal});
-    	  //postID++;
       }
     //maxlength 100 characters al defined in index.jade  
+	  }); //einde van post button onclick
 
-	}); //einde van post button onclick
+  //get all posts
+  var allPostsSubscription = client.subscribe('/allposts', function(vote) {
+      //elke vote hoort bij een index
 
-	//on click van vraag, maak groter
-  $('.vote').on('click', function()
-  {
-      var voteParent = this.parent();
-      console.log("clicked");
+      //voor elke .vote gaan we overlopen of de index van de vote waar 
+      //op geklikt is overeenkomt met die van de post
 
-      //GAAT NI, WANT DN VOTE DIE GWN ALLE MSGES, TOCH NOG MET ID WERKEN
+      //als die hetzelfde is gaan we die size groter maken
 
-      //var votePublication = client.publish('/vote', {vote : voteParent});
   });
+
+    //click on .vote within .bullet
+    $(".bulletin").on("click",".vote",function(event){
+      //index verkrijgen van welke .vote we moeten publishen
+
+      //publish met index
+
+    });//einde van vote button on click
 
 });
