90206432383c133097e9a3013f6a2e972c6b503e
tommooredotbe <mooretom@outlook.com>
2014-04-26 20:22:07 +0200

    gulp

--- project_x/gulpfile.js
+++ project_x/gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
 var concatCss = require('gulp-concat-css');
 var minifyCSS = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
+var pngcrush = require('imagemin-pngcrush');
+/*var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    gulp.src('public/stylesheets/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/stylesheets/css'));
+});*/
 
 gulp.task('styles', function () {
   //concatcss
-  gulp.src('public/stylesheets/css/*.css')
+  gulp.src('public/stylesheets/**/*.css')
     .pipe(concatCss("build.css"))
     .pipe(minifyCSS(opts))
     .pipe(gulp.dest('public/build/css/'));
@@ -16,7 +25,7 @@ gulp.task('images', function () {
     .pipe(imagemin({
         use: [pngcrush()]
     }))
-    .pipe(gulp.dest('build/assets/'));
+    .pipe(gulp.dest('public/build/assets/'));
 });
 
 gulp.task('default', ['styles','images']);
